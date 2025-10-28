feaaecac2c27ff86c04ad7fbaebeb0eeaa7b7319
fazamumtaz <fazamumtazramadhan@gmail.com>
2025-10-28 22:54:56 +0700

    feat: finish landing page (unresponsive)

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,61 @@
+import HeaderLanding from "@/components/HeaderLanding";
+import React from "react";
+import { Marquee } from "@/components/ui/marquee";
+import EachUtils from "@/utils/EachUtils";
+import { LIST_COURSES } from "@/constants/listCourses";
+import { LIST_HERO } from "@/constants/listHero";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const LandingPage = () => {
   return (
-    <div className="flex flex-col w-screen h-screen items-center justify-center">
-      <h1 className="font-extrabold text-xl text-center">
-        DEV NYA MASIH NGANTUK, LAGI BOBO, NTAR LAGI YAK
-      </h1>
-      <p>Coba tanya badzlan, gimana cara jago</p>
+    <div className="pt-16">
+      <HeaderLanding />
+      <div className="h-[calc(100vh-144px)] flex flex-col gap-y-2 items-center max-w-[1036px] mx-auto justify-center">
+        <EachUtils
+          of={LIST_HERO}
+          render={(item) => (
+            <>
+              <h1 className="font-extrabold mb-5 leading-11 text-neutral-700 text-4xl text-center">
+                {item.textHero}
+              </h1>
+              <Button className="w-55" variant={"primary"}>
+                {item.signUpBtn}
+              </Button>
+              <Button className="w-55" variant={"sidebar"}>
+                {item.loginBtn}
+              </Button>
+            </>
+          )}
+        />
+      </div>
+      <div className="h-20 border-2 border-steal-200 flex items-center">
+        <Marquee
+          className="w-[1036px] mx-auto [--gap:4rem] [--duration:10s]"
+          pauseOnHover
+        >
+          <div className="flex items-center gap-x-12">
+            <EachUtils
+              of={LIST_COURSES}
+              render={(item) => (
+                <div key={item.id} className="flex flex-row items-center">
+                  <Image
+                    key={item.id}
+                    alt={item.subject}
+                    width={40}
+                    height={40}
+                    src={item.iconSrc}
+                    className="mr-4"
+                  />
+                  <span className="text-lg font-bold">{item.subject}</span>
+                </div>
+              )}
+            />
+          </div>
+        </Marquee>
+      </div>
     </div>
   );
-}
+};
+
+export default LandingPage;
---
+++ src/constants/listHero.js
@@ -0,0 +1,9 @@
+export const LIST_HERO = [
+  {
+    image: "",
+    textHero:
+      "Belajar jadi seru, jelajahi materi interaktif dengan pengalaman Augmented Reality (AR) yang bikin kamu ngerasa ada di dalam pelajaran!",
+    signUpBtn: "Mulai",
+    loginBtn: "aku Sudah punya akun",
+  },
+];
