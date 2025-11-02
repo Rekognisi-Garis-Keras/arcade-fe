df29737ebaf3d814479a986a5a65962f461f18aa
fazamumtaz <fazamumtazramadhan@gmail.com>
2025-11-03 01:49:27 +0700

    fix: responsive profile, ring n selection in input

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,30 +2,30 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/UI/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogFooter,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
+} from "@/components/UI/dialog";
+import { Input } from "@/components/UI/input";
+import { Textarea } from "@/components/UI/textarea";
 import { Upload, SquarePen } from "lucide-react";
 
 export default function ProfilePage() {
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isEditBioOpen, setIsEditBioOpen] = useState(false);
   const [name, setName] = useState("Eko Kurniawan Khannedy");
   const [email, setEmail] = useState("ekokhannedy@gmail.com");
   const [telp, setTelp] = useState("+62 821 1010 8754");
   const [bio, setBio] = useState("Love to learn with AR!");
 
   return (
-    <main className="p-10 w-full min-h-screen">
+    <main className="p-10 w-full min-h-screen pb-20">
       {/* profile picture card */}
-      <div className="flex border-2 border-b-4 rounded-xl p-5 gap-5 items-center">
+      <div className="flex md:flex-row flex-col border-2 border-b-4 rounded-xl p-5 gap-5 items-center">
         <Image
           src="/profile.jpg"
           alt="Profile picture"
@@ -33,7 +33,7 @@ export default function ProfilePage() {
           height={100}
           className="rounded-full border-4 border-sky-500"
         />
-        <div className="flex flex-col items-start h-full justify-between gap-y-3">
+        <div className="flex flex-col md:items-start items-center h-full justify-between gap-y-3">
           <h1 className="text-lg font-bold">Profile Picture</h1>
 
           <Button variant={"primary"} size={"sm"}>
@@ -49,17 +49,17 @@ export default function ProfilePage() {
         </div>
       </div>
 
-      <hr className="my-5 border-2 rounded-full mx-auto w-[80%]" />
+      <hr className="my-5 border-2 rounded-full" />
 
       {/* informasi pribadi card */}
       <div className="border-2 border-b-4 rounded-xl p-5 pb-10 mb-5">
-        <div className="flex justify-between mb-5">
+        <div className="flex justify-between mb-5 items-center">
           <h1 className="text-lg font-bold">Informasi Pribadi</h1>
           <Button onClick={() => setIsEditOpen(true)}>
             <SquarePen />
           </Button>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-2 md:flex-row flex-col gap-y-3">
           <div className="w-full">
             <h3 className="font-semibold ">Nama</h3>
             <p className="text-sm text-gray-500">{name}</p>
@@ -77,14 +77,19 @@ export default function ProfilePage() {
 
       {/* bio card */}
       <div className="w-full rounded-xl p-5 border-2 border-b-4">
-        <h1 className="text-lg font-bold mb-3">Bio</h1>
+        <div className="flex justify-between mb-5 items-center">
+          <h1 className="text-lg font-bold">Bio</h1>
+          <Button onClick={() => setIsEditBioOpen(true)}>
+            <SquarePen />
+          </Button>
+        </div>
         <p className="text-gray-500">{bio}</p>
       </div>
 
       <Dialog open={isEditOpen} onOpenChange={setIsEditOpen}>
         <DialogContent className="max-w-md">
           <DialogHeader>
-            <DialogTitle>Edit Profil ✏️</DialogTitle>
+            <DialogTitle>Edit Informasi Pribadi ✏️</DialogTitle>
           </DialogHeader>
           <div className="space-y-4 py-3">
             <div>
@@ -105,6 +110,21 @@ export default function ProfilePage() {
               </label>
               <Input value={telp} onChange={(e) => setTelp(e.target.value)} />
             </div>
+          </div>
+          <DialogFooter>
+            <Button onClick={() => setIsEditOpen(false)} variant={"secondary"}>
+              Simpan
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={isEditBioOpen} onOpenChange={setIsEditBioOpen}>
+        <DialogContent className="max-w-md">
+          <DialogHeader>
+            <DialogTitle>Edit Bio ✏️</DialogTitle>
+          </DialogHeader>
+          <div className="space-y-4 py-3">
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
                 Bio
@@ -117,7 +137,10 @@ export default function ProfilePage() {
             </div>
           </div>
           <DialogFooter>
-            <Button onClick={() => setIsEditOpen(false)} variant={"secondary"}>
+            <Button
+              onClick={() => setIsEditBioOpen(false)}
+              variant={"secondary"}
+            >
               Simpan
             </Button>
           </DialogFooter>
