29d872675472b1b6ac8566fb1c24078e009a9c53
fazamumtaz <fazamumtazramadhan@gmail.com>
2025-11-03 01:25:44 +0700

    fix: rebuild profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -13,58 +13,73 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Upload } from "lucide-react";
+import { Upload, SquarePen } from "lucide-react";
 
 export default function ProfilePage() {
   const [isEditOpen, setIsEditOpen] = useState(false);
   const [name, setName] = useState("Eko Kurniawan Khannedy");
+  const [email, setEmail] = useState("ekokhannedy@gmail.com");
+  const [telp, setTelp] = useState("+62 821 1010 8754");
   const [bio, setBio] = useState("Love to learn with AR!");
 
   return (
-    <div className="min-h-screen">
-      <main className="flex-1 p-10 flex flex-col items-center">
-        <h1 className="text-2xl font-semibold text-gray-800 mb-6">
-          Profil Kamu 👤
-        </h1>
+    <main className="p-10 w-full min-h-screen">
+      {/* profile picture card */}
+      <div className="flex border-2 border-b-4 rounded-xl p-5 gap-5 items-center">
+        <Image
+          src="/profile.jpg"
+          alt="Profile picture"
+          width={100}
+          height={100}
+          className="rounded-full border-4 border-sky-500"
+        />
+        <div className="flex flex-col items-start h-full justify-between gap-y-3">
+          <h1 className="text-lg font-bold">Profile Picture</h1>
 
-        <Card className="w-full max-w-lg rounded-xl border-2 border-b-4 border-gray-200 bg-white">
-          <CardContent className="flex flex-col items-center p-6 space-y-5">
-            <div className="relative">
-              <Image
-                src="/profile.jpg"
-                alt="Profile picture"
-                width={100}
-                height={100}
-                className="rounded-full border-4 border-sky-500"
-              />
-              <Button size={"uploadButton"} variant={"uploadButton"}>
-                <Upload className="h-3! w-3!" />
-              </Button>
-            </div>
+          <Button variant={"primary"} size={"sm"}>
+            <Upload />
+            Upload foto kamu
+          </Button>
+          <div>
+            <p className="text-xs text-gray-400">
+              File yang disupport: .jpg .png .jpeg
+            </p>
+            <p className="text-xs text-gray-400">File maksimal berukuran 1Mb</p>
+          </div>
+        </div>
+      </div>
 
-            <div className="text-center">
-              <h2 className="text-lg font-bold text-gray-800">{name}</h2>
-              <p className="text-sm text-gray-500">Bergabung pada Juni 2025</p>
-            </div>
+      <hr className="my-5 border-2 rounded-full mx-auto w-[80%]" />
 
-            {/* Bio Section */}
-            <div className="w-full text-center">
-              <h3 className="text-sm font-semibold text-gray-700 mb-2">
-                Bio 📝
-              </h3>
-              <p className="text-gray-600 text-sm leading-relaxed">{bio}</p>
-            </div>
+      {/* informasi pribadi card */}
+      <div className="border-2 border-b-4 rounded-xl p-5 pb-10 mb-5">
+        <div className="flex justify-between mb-5">
+          <h1 className="text-lg font-bold">Informasi Pribadi</h1>
+          <Button onClick={() => setIsEditOpen(true)}>
+            <SquarePen />
+          </Button>
+        </div>
+        <div className="flex gap-2">
+          <div className="w-full">
+            <h3 className="font-semibold ">Nama</h3>
+            <p className="text-sm text-gray-500">{name}</p>
+          </div>
+          <div className="w-full">
+            <h3 className="font-semibold ">Email</h3>
+            <p className="text-sm text-gray-500">{email}</p>
+          </div>
+          <div className="w-full">
+            <h3 className="font-semibold ">Nomor Telepon</h3>
+            <p className="text-sm text-gray-500">{telp}</p>
+          </div>
+        </div>
+      </div>
 
-            {/* Edit Button */}
-            <Button
-              onClick={() => setIsEditOpen(true)}
-              className="mt-2 bg-sky-500 hover:bg-sky-600 border-none cursor-pointer text-white rounded-lg"
-            >
-              Edit Profil
-            </Button>
-          </CardContent>
-        </Card>
-      </main>
+      {/* bio card */}
+      <div className="w-full rounded-xl p-5 border-2 border-b-4">
+        <h1 className="text-lg font-bold mb-3">Bio</h1>
+        <p className="text-gray-500">{bio}</p>
+      </div>
 
       <Dialog open={isEditOpen} onOpenChange={setIsEditOpen}>
         <DialogContent className="max-w-md">
@@ -80,6 +95,18 @@ export default function ProfilePage() {
             </div>
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
+                Email
+              </label>
+              <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Nomor Telepon
+              </label>
+              <Input value={telp} onChange={(e) => setTelp(e.target.value)} />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
                 Bio
               </label>
               <Textarea
@@ -90,15 +117,12 @@ export default function ProfilePage() {
             </div>
           </div>
           <DialogFooter>
-            <Button
-              onClick={() => setIsEditOpen(false)}
-              className="bg-blue-500 hover:bg-blue-600 text-white"
-            >
+            <Button onClick={() => setIsEditOpen(false)} variant={"secondary"}>
               Simpan
             </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
-    </div>
+    </main>
   );
 }
